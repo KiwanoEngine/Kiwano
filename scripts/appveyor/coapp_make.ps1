b69062f20a6ace1b67ccabd2d7fae3f63f03dbe1
Nomango <569629550@qq.com>
2019-08-08 15:20:02 +0800

    [build] update README.md and fix appveyor.yml

--- scripts/appveyor/coapp_make.ps1
+++ scripts/appveyor/coapp_make.ps1
@@ -1,3 +1,25 @@
+. .\scripts\appveyor\appveyor_get_build.ps1
+. .\scripts\appveyor\appveyor_get_artifacts.ps1
+
+# get job artifacts
+Write-Host "Start to download artifacts from other jobs"
+
+(Get-AppVeyorBuild).build.jobs | foreach-object {
+    $jobId = $_.jobId
+    if ($jobId -ne $env:APPVEYOR_JOB_ID) {
+        # Get job artifacts information
+        (Get-AppVeyorArtifacts -Job $jobId) | foreach-object {
+            # Create directory if not exists
+            $filePath = $_.fileName.Substring(0, $_.fileName.LastIndexOf('/'))
+            if (!(Test-Path -Path $filePath)) {
+                New-Item -ItemType "directory" -Path $filePath
+            }
+            # Download artifact from other job
+            Start-FileDownload "https://ci.appveyor.com/api/buildjobs/$jobId/artifacts/$($_.fileName)" -FileName $_.fileName
+        }
+    };
+}
+
 Write-Host "Start to build nupkg files"
 
 # This is the CoApp .autopkg file to create.
