afbb0116d1e05b3654669f30ef149fb2025f6b3e
Nomango <569629550@qq.com>
2019-08-09 09:41:54 +0800

    [deploy] fix appveyor configuration

--- scripts/appveyor/appveyor_get_build.ps1
+++ scripts/appveyor/appveyor_get_build.ps1
@@ -8,7 +8,7 @@ function Get-AppVeyorBuild {
         throw "not an appveyor build."
     }
 
-    return Invoke-RestMethod -Method GET -Uri "https://ci.appveyor.com/api/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG" -Headers @{
+    return Invoke-RestMethod -Method GET -Uri "https://ci.appveyor.com/api/projects/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/build/$env:APPVEYOR_BUILD_VERSION" -Headers @{
         "Authorization" = "Bearer $env:APPVEYOR_API_TOKEN"
         "Content-type"  = "application/json"
     }
