5050f7028aef87c379d3ba7e55dbbb07ee521213
Nomango <569629550@qq.com>
2019-08-18 23:19:51 +0800

    [deploy] update scripts

--- scripts/appveyor/clear_project_configuration.ps1
+++ scripts/appveyor/clear_project_configuration.ps1
@@ -4,5 +4,6 @@ $replaceTo = "<DebugInformationFormat>None</DebugInformationFormat>"
 Get-ChildItem -Path 'projects\' *.vcxproj | ForEach-Object {
     $filePath = 'projects\' + $_
     Copy-Item -Path $filePath -Destination ($filePath + '.template')
-    Get-Content ($filePath + '.template') | ForEach-Object { $_ -replace $replace, $replaceTo } > $filePath
+    Get-Content ($filePath + '.template') -Encoding UTF8 | ForEach-Object { $_ -replace $replace, $replaceTo } | Out-File $filePath -Encoding UTF8
+    Remove-Item -Path ($filePath + '.template')
 }
