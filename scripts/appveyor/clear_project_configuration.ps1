955bf22740b8ded3c21ea33a68a402d098fa3740
Nomango <nomango@qq.com>
2019-10-08 14:25:33 +0800

    update project files

--- scripts/appveyor/clear_project_configuration.ps1
+++ scripts/appveyor/clear_project_configuration.ps1
@@ -1,15 +1,27 @@
-$replace = "<DebugInformationFormat>EditAndContinue</DebugInformationFormat>"
-$replaceTo = "<DebugInformationFormat>None</DebugInformationFormat>"
+function Set-FileConfiguration {
+    param(
+        [string]$filePath
+    )
 
-Get-ChildItem -Path 'projects\' *.vcxproj | ForEach-Object {
-    $filePath = 'projects\' + $_
+    $replace1 = "<DebugInformationFormat>EditAndContinue</DebugInformationFormat>"
+    $replace2 = "<DebugInformationFormat>ProgramDatabase</DebugInformationFormat>"
+    $replaceTo = "<DebugInformationFormat>None</DebugInformationFormat>"
 
     # Create a copy of .vcxproj file
     Copy-Item -Path $filePath -Destination ($filePath + '.template')
 
     # Overlay some configurations
-    Get-Content ($filePath + '.template') -Encoding UTF8 | ForEach-Object { $_ -replace $replace, $replaceTo } | Out-File $filePath -Encoding UTF8
+    Get-Content ($filePath + '.template') -Encoding UTF8 | ForEach-Object { $_ -replace $replace1, $replaceTo; $_ -replace $replace2, $replaceTo; } | Out-File $filePath -Encoding UTF8
 
     # Delete the copy file
     Remove-Item -Path ($filePath + '.template')
+}
+
+Get-ChildItem -Path 'projects\' -Directory | ForEach-Object {
+    $dirPath = "projects\$($_)"
+
+    # Search all vcxproj files
+    Get-ChildItem -Path $dirPath *.vcxproj -File | ForEach-Object {
+        Set-FileConfiguration $dirPath + $_
+    }
 }
